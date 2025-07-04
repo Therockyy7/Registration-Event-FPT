4ff1a20db1150d7002d52869d77a404af4de9c24
tran nguyen hoang phuc <tphuc9398@gmail.com>
2025-07-04 08:37:53 +0700

    second  commit

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/Event_Management_System')
+        await mongoose.connect('mongodb+srv://HoangPhuc:phuctran2004%40@cluster0.5z1efea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log("Connect to MongoDB");
 
     } catch (error) {
